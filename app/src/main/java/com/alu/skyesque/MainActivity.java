e67f42651607275c8824fe95886f2895cc03199c
githiomi <danielgithiomi@gmail.com>
2024-03-18 11:02:30 +0400

    Added splash screen delay

--- app/src/main/java/com/alu/skyesque/MainActivity.java
+++ app/src/main/java/com/alu/skyesque/MainActivity.java
@@ -1,6 +1,8 @@
 package com.alu.skyesque;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(MainActivity.this, HomeActivity.class));
+            finish();
+        }, 3000);
     }
 }

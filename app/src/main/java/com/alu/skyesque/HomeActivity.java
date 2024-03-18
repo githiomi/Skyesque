5eb22bf89e59217d85213de58f3f5f7ed0b756f9
githiomi <danielgithiomi@gmail.com>
2024-03-18 11:05:31 +0400

    Updated project to use view binding

--- app/src/main/java/com/alu/skyesque/HomeActivity.java
+++ app/src/main/java/com/alu/skyesque/HomeActivity.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.alu.skyesque.databinding.ActivityHomeBinding;
+
+import java.time.LocalDate;
 
 public class HomeActivity extends AppCompatActivity {
 

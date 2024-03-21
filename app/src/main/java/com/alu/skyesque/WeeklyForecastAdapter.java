ecd130ae70e3d6a1f294b8acd3b1715805d4fd53
githiomi <danielgithiomi@gmail.com>
2024-03-21 13:33:53 +0400

    Modified weekly summary adapter to show days of week

--- app/src/main/java/com/alu/skyesque/WeeklyForecastAdapter.java
+++ app/src/main/java/com/alu/skyesque/WeeklyForecastAdapter.java
@@ -1,18 +1,24 @@
 package com.alu.skyesque;
 
 import android.content.Context;
+import android.icu.util.LocaleData;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.time.LocalDate;
+
 public class WeeklyForecastAdapter extends RecyclerView.Adapter<WeeklyForecastAdapter.ViewHolder> {
 
     // Adapter properties
+    String[] weekdays = new String[7];
+
     Context context;
 
     public WeeklyForecastAdapter(Context context) {
@@ -32,6 +38,8 @@ public class WeeklyForecastAdapter extends RecyclerView.Adapter<WeeklyForecastAd
             holder.weeklySummaryItem.setBackground(ContextCompat.getDrawable(this.context, R.drawable.summary_item_active_background));
         }
 
+        holder.dayOfWeek.setText(LocalDate.now().plusDays(position).getDayOfWeek().toString());
+
     }
 
     @Override
@@ -43,10 +51,12 @@ public class WeeklyForecastAdapter extends RecyclerView.Adapter<WeeklyForecastAd
 
         // Views
         RelativeLayout weeklySummaryItem;
+        TextView dayOfWeek;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             this.weeklySummaryItem = itemView.findViewById(R.id.RL_weeklyForecast);
+            this.dayOfWeek = itemView.findViewById(R.id.TV_dayOfTheWeek);
         }
     }
 }

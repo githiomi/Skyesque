44e5f47418e4fa865ccaa650a48bc909ee1baa2a
githiomi <danielgithiomi@gmail.com>
2024-03-25 09:55:39 +0400

    Changed air quality to wind direction

---
+++ app/src/main/java/com/alu/skyesque/models/WeatherDTO.java
@@ -0,0 +1,20 @@
+package com.alu.skyesque.models;
+
+/**
+ * Author: dangit
+ * Project Name: Skyesque
+ * Name: Daniel Githiomi
+ * Student Id: S2110911
+ * Programme of Study: Computing
+ * Date of creation: Monday 25, 03 2024
+ */
+public class WeatherDTO {
+    private String day;
+    private String weatherSummary;
+    private String temperature;
+    private String windSpeed;
+    private String humidity;
+    private String pressure;
+    private String visibility;
+
+}

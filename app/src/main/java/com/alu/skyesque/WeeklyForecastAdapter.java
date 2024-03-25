653a30f3ff5f190fc6de6c497ae9f9e64a78aaf9
githiomi <danielgithiomi@gmail.com>
2024-03-25 19:10:03 +0400

    Successfully render 3 day forecast using adapter

--- app/src/main/java/com/alu/skyesque/WeeklyForecastAdapter.java
+++ app/src/main/java/com/alu/skyesque/WeeklyForecastAdapter.java
@@ -5,6 +5,7 @@ import android.icu.util.LocaleData;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -13,9 +14,12 @@ import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.alu.skyesque.models.DetailedWeatherDTO;
+import com.alu.skyesque.models.WeatherDTO;
+import com.alu.skyesque.models.WeatherUnit;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Name                 Daniel Githiomi
@@ -26,6 +30,8 @@ public class WeeklyForecastAdapter extends RecyclerView.Adapter<WeeklyForecastAd
 
     // Adapter properties
     List<DetailedWeatherDTO> weatherDTOs;
+    int[] weatherIcons = {R.drawable.cloudy, R.drawable.thunder, R.drawable.fog, R.drawable.mist, R.drawable.rain, R.drawable.snow, R.drawable.day_clear, R.drawable.night_sleet, R.drawable.day_rain, R.drawable.snow_thunder};
+
     Context context;
 
     public WeeklyForecastAdapter(Context context, List<DetailedWeatherDTO> weatherDTOs) {
@@ -42,6 +48,8 @@ public class WeeklyForecastAdapter extends RecyclerView.Adapter<WeeklyForecastAd
     @Override
     public void onBindViewHolder(@NonNull WeeklyForecastAdapter.ViewHolder holder, int position) {
 
+        DetailedWeatherDTO weatherDTO = weatherDTOs.get(position);
+
         if (position == 0) {
             holder.weeklySummaryItem.setBackground(ContextCompat.getDrawable(this.context, R.drawable.summary_item_active_background));
         } else {
@@ -49,6 +57,9 @@ public class WeeklyForecastAdapter extends RecyclerView.Adapter<WeeklyForecastAd
         }
 
         holder.dayOfWeek.setText(LocalDate.now().plusDays(position).getDayOfWeek().toString());
+        holder.weatherIcon.setImageResource(this.weatherIcons[new Random().nextInt((10 - 1) + 1)]);
+        holder.minimumTemperature.setText(weatherDTO.getMinimumTemperature());
+        holder.maximumTemperature.setText(weatherDTO.getMaximumTemperature());
 
     }
 
@@ -61,12 +72,16 @@ public class WeeklyForecastAdapter extends RecyclerView.Adapter<WeeklyForecastAd
 
         // Views
         RelativeLayout weeklySummaryItem;
-        TextView dayOfWeek;
+        TextView dayOfWeek, minimumTemperature, maximumTemperature;
+        ImageView weatherIcon;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             this.weeklySummaryItem = itemView.findViewById(R.id.RL_weeklyForecast);
             this.dayOfWeek = itemView.findViewById(R.id.TV_dayOfTheWeek);
+            this.weatherIcon = itemView.findViewById(R.id.IV_dailyWeatherImage);
+            this.minimumTemperature = itemView.findViewById(R.id.TV_minimumTemperature);
+            this.maximumTemperature = itemView.findViewById(R.id.TV_maximumTemperature);
         }
     }
 }

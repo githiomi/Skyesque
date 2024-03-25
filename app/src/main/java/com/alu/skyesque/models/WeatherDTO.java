914d4c212a90896c5ef73e933ee078347d6751c2
githiomi <danielgithiomi@gmail.com>
2024-03-25 10:02:32 +0400

    Created weather DTO

--- app/src/main/java/com/alu/skyesque/models/WeatherDTO.java
+++ app/src/main/java/com/alu/skyesque/models/WeatherDTO.java
@@ -9,12 +9,34 @@ package com.alu.skyesque.models;
  * Date of creation: Monday 25, 03 2024
  */
 public class WeatherDTO {
+    private String location;
     private String day;
     private String weatherSummary;
     private String temperature;
+    private String windDirection;
     private String windSpeed;
     private String humidity;
     private String pressure;
     private String visibility;
+    private String latitude;
+    private String longitude;
 
+    public WeatherDTO() {
+    }
+
+    public WeatherDTO(String location, String day, String weatherSummary, String temperature, String windDirection,
+                      String windSpeed, String humidity, String pressure, String visibility, String latitude,
+                      String longitude) {
+        this.location = location;
+        this.day = day;
+        this.weatherSummary = weatherSummary;
+        this.temperature = temperature;
+        this.windDirection = windDirection;
+        this.windSpeed = windSpeed;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        this.visibility = visibility;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 }

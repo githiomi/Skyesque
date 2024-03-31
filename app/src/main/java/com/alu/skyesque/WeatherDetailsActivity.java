83a61e0dceea33ad58c2525aeedd09ddbabc4d10
githiomi <danielgithiomi@gmail.com>
2024-03-31 23:34:29 +0400

    Can successfully parse data to details activity

--- app/src/main/java/com/alu/skyesque/WeatherDetailsActivity.java
+++ app/src/main/java/com/alu/skyesque/WeatherDetailsActivity.java
@@ -2,9 +2,9 @@ package com.alu.skyesque;
 
 import static android.content.ContentValues.TAG;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,11 +13,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.alu.skyesque.models.DetailedWeatherDTO;
-import com.alu.skyesque.models.WeatherDTO;
 
-import java.util.Objects;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Random;
 
 public class WeatherDetailsActivity extends AppCompatActivity {
+
+    // Views
+    TextView townName, pinLocation, currentDate, currentTemperature, minMaxTemperature, currentState,
+            windSpeedSummary, humiditySummary, pressureSummary, windDirection, uvIndex, rainfall, visibility;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +35,75 @@ public class WeatherDetailsActivity extends AppCompatActivity {
             return insets;
         });
 
-        WeatherDTO detailedWeatherDTO = getIntent().getParcelableExtra("toDetailsDTO");
+        // Bind views
+        bindViews();
+
+        DetailedWeatherDTO detailedWeatherDTO = getIntent().getParcelableExtra("toDetailsDTO");
         Log.e(TAG, "onCreate: " + detailedWeatherDTO.toString());
+
+        populateData(detailedWeatherDTO);
+
+        // CLick listeners
+        this.pinLocation.setOnClickListener(v -> {
+//            Intent mapIntent = new Intent(this, MapActivity.class);
+//            String coordinates = detailedWeatherDTO.getLatitude() + "," + detailedWeatherDTO.getLongitude();
+//            mapIntent.putExtra("coordinates", coordinates);
+//            startActivity(mapIntent);
+        });
+
+    }
+
+    private void bindViews() {
+        this.townName = findViewById(R.id.TV_townName);
+        this.pinLocation = findViewById(R.id.TV_pinLocation);
+        this.currentDate = findViewById(R.id.TV_currentDate);
+        this.currentTemperature = findViewById(R.id.TV_currentTemperature);
+        this.minMaxTemperature = findViewById(R.id.TV_minMaxTemperature);
+        this.currentState = findViewById(R.id.TV_currentState);
+        this.windSpeedSummary = findViewById(R.id.TV_windSummary);
+        this.humiditySummary = findViewById(R.id.TV_humiditySummary);
+        this.pressureSummary = findViewById(R.id.TV_sunSummary);
+        this.windDirection = findViewById(R.id.TV_windDirection);
+        this.uvIndex = findViewById(R.id.TV_uvIndexDesc);
+        this.rainfall = findViewById(R.id.TV_rainfallDesc);
+        this.visibility = findViewById(R.id.TV_visibilityDesc);
+    }
+
+    private void populateData(DetailedWeatherDTO dto) {
+
+        this.townName.setText("Glasgow");
+        this.currentDate.setText(getDate(dto.getDay()));
+        this.currentTemperature.setText(dto.getTemperatureCelsius());
+        String minMax = dto.getMinimumTemperature() + " / " + dto.getMaximumTemperature();
+        this.minMaxTemperature.setText(minMax);
+        this.currentState.setText(dto.getWeatherSummary());
+        this.windSpeedSummary.setText(dto.getWindSpeed());
+        this.humiditySummary.setText(dto.getHumidity());
+        this.pressureSummary.setText(dto.getPressure());
+        this.windDirection.setText(dto.getWindDirection());
+        this.uvIndex.setText(getUvIndex(dto.getUvRisk()));
+        this.rainfall.setText(getRainfall());
+        this.visibility.setText(dto.getVisibility());
+
+    }
+
+    private String getDate(String day) {
+        LocalDate localDate = LocalDate.now();
+        return day.toUpperCase() + " " + localDate.getDayOfMonth() + " " + localDate.getMonth() + ", " + localDate.getYear();
+    }
+
+    private String getUvIndex(String uv) {
+        return Integer.parseInt(uv) <= 5 ? uv + " - Low" : uv + " - High";
+    }
+
+    private String getRainfall() {
+        float min = 20.0F, max = 300.0F;
+        return String.valueOf(round(new Random().nextFloat() * (max - min) + min, 2)) + "mm";
+    }
+
+    private String round(float d, int decimalPlace) {
+        BigDecimal bd = new BigDecimal(Float.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+        return String.valueOf(bd);
     }
 }
--- app/src/main/java/com/alu/skyesque/models/DetailedWeatherDTO.java
+++ app/src/main/java/com/alu/skyesque/models/DetailedWeatherDTO.java
@@ -1,6 +1,9 @@
 package com.alu.skyesque.models;
 
+import android.os.Parcel;
 import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
 
 /**
  * Author: dangit
@@ -11,7 +14,19 @@ import android.os.Parcelable;
  * Date of creation: Monday 25, 03 2024
  */
 
-public class DetailedWeatherDTO extends WeatherDTO implements Parcelable {
+public class DetailedWeatherDTO implements Parcelable {
+    private String location;
+    private String day;
+    private String weatherSummary;
+    private String temperatureCelsius;
+    private String temperatureFahrenheit;
+    private String windDirection;
+    private String windSpeed;
+    private String humidity;
+    private String pressure;
+    private String visibility;
+    private String latitude;
+    private String longitude;
     private String minimumTemperature;
     private String maximumTemperature;
     private String uvRisk;
@@ -28,86 +43,180 @@ public class DetailedWeatherDTO extends WeatherDTO implements Parcelable {
                               String longitude, String minimumTemperature, String maximumTemperature,
                               String uvRisk, String pollution, String sunrise, String sunset) {
 
-        super(location, day, weatherSummary, temperatureCelsius, temperatureFahrenheit, windDirection,
-                windSpeed, humidity, pressure, visibility, latitude, longitude);
-
+        this.location = location;
+        this.day = day;
+        this.weatherSummary = weatherSummary;
+        this.temperatureCelsius = temperatureCelsius;
+        this.temperatureFahrenheit = temperatureFahrenheit;
+        this.windDirection = windDirection;
+        this.windSpeed = windSpeed;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        this.visibility = visibility;
+        this.latitude = latitude;
+        this.longitude = longitude;
         this.minimumTemperature = minimumTemperature;
         this.maximumTemperature = maximumTemperature;
         this.uvRisk = uvRisk;
         this.pollution = pollution;
         this.sunrise = sunrise;
         this.sunset = sunset;
+    }
+
+    protected DetailedWeatherDTO(Parcel in) {
+        location = in.readString();
+        day = in.readString();
+        weatherSummary = in.readString();
+        temperatureCelsius = in.readString();
+        temperatureFahrenheit = in.readString();
+        windDirection = in.readString();
+        windSpeed = in.readString();
+        humidity = in.readString();
+        pressure = in.readString();
+        visibility = in.readString();
+        latitude = in.readString();
+        longitude = in.readString();
+        minimumTemperature = in.readString();
+        maximumTemperature = in.readString();
+        uvRisk = in.readString();
+        pollution = in.readString();
+        sunrise = in.readString();
+        sunset = in.readString();
+    }
+
+    public static final Creator<DetailedWeatherDTO> CREATOR = new Creator<DetailedWeatherDTO>() {
+        @Override
+        public DetailedWeatherDTO createFromParcel(Parcel in) {
+            return new DetailedWeatherDTO(in);
+        }
+
+        @Override
+        public DetailedWeatherDTO[] newArray(int size) {
+            return new DetailedWeatherDTO[size];
+        }
+    };
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public String getWeatherSummary() {
+        return weatherSummary;
+    }
+
+    public String getTemperatureCelsius() {
+        return temperatureCelsius;
+    }
+
+    public String getTemperatureFahrenheit() {
+        return temperatureFahrenheit;
+    }
+
+    public String getWindDirection() {
+        return windDirection;
+    }
+
+    public String getWindSpeed() {
+        return windSpeed;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public String getPressure() {
+        return pressure;
+    }
+
+    public String getVisibility() {
+        return visibility;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
     }
 
     public String getMinimumTemperature() {
         return minimumTemperature;
     }
 
-    public void setMinimumTemperature(String minimumTemperature) {
-        this.minimumTemperature = minimumTemperature;
-    }
-
     public String getMaximumTemperature() {
         return maximumTemperature;
-    }
-
-    public void setMaximumTemperature(String maximumTemperature) {
-        this.maximumTemperature = maximumTemperature;
     }
 
     public String getUvRisk() {
         return uvRisk;
     }
 
-    public void setUvRisk(String uvRisk) {
-        this.uvRisk = uvRisk;
-    }
-
     public String getPollution() {
         return pollution;
-    }
-
-    public void setPollution(String pollution) {
-        this.pollution = pollution;
     }
 
     public String getSunrise() {
         return sunrise;
     }
 
-    public void setSunrise(String sunrise) {
-        this.sunrise = sunrise;
-    }
-
     public String getSunset() {
         return sunset;
     }
 
-    public void setSunset(String sunset) {
-        this.sunset = sunset;
-    }
-
+    @NonNull
     @Override
     public String toString() {
         return "DetailedWeatherDTO{" +
-                "  location='" + super.getLocation() + '\'' +
-                ", day='" + super.getDay() + '\'' +
-                ", weatherSummary='" + super.getWeatherSummary() + '\'' +
-                ", temperatureCelsius='" + super.getTemperatureCelsius() + '\'' +
-                ", temperatureFahrenheit='" + super.getTemperatureFahrenheit() + '\'' +
-                ", windDirection='" + super.getWindDirection() + '\'' +
-                ", windSpeed='" + super.getWindSpeed() + '\'' +
-                ", humidity='" + super.getHumidity() + '\'' +
-                ", pressure='" + super.getPressure() + '\'' +
-                ", visibility='" + super.getVisibility() + '\'' +
-                ", latitude='" + super.getLatitude() + '\'' +
-                ", longitude='" + super.getLongitude() + '\'' +
-                ", minimumTemperature='" + minimumTemperature + '\'' +
-                ", maximumTemperature='" + maximumTemperature + '\'' +
-                ", uvRisk='" + uvRisk + '\'' +
-                ", pollution='" + pollution + '\'' +
-                ", sunrise='" + sunrise + '\'' +
-                ", sunset='" + sunset + '\'' +
+                "  location='" + getLocation() + '\'' +
+                ", day='" + getDay() + '\'' +
+                ", weatherSummary='" + getWeatherSummary() + '\'' +
+                ", temperatureCelsius='" + getTemperatureCelsius() + '\'' +
+                ", temperatureFahrenheit='" + getTemperatureFahrenheit() + '\'' +
+                ", windDirection='" + getWindDirection() + '\'' +
+                ", windSpeed='" + getWindSpeed() + '\'' +
+                ", humidity='" + getHumidity() + '\'' +
+                ", pressure='" + getPressure() + '\'' +
+                ", visibility='" + getVisibility() + '\'' +
+                ", latitude='" + getLatitude() + '\'' +
+                ", longitude='" + getLongitude() + '\'' +
+                ", minimumTemperature='" + getMinimumTemperature() + '\'' +
+                ", maximumTemperature='" + getMaximumTemperature() + '\'' +
+                ", uvRisk='" + getUvRisk() + '\'' +
+                ", pollution='" + getPollution() + '\'' +
+                ", sunrise='" + getSunrise() + '\'' +
+                ", sunset='" + getSunset() + '\'' +
                 '}';
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeString(location);
+        dest.writeString(day);
+        dest.writeString(weatherSummary);
+        dest.writeString(temperatureCelsius);
+        dest.writeString(temperatureFahrenheit);
+        dest.writeString(windDirection);
+        dest.writeString(windSpeed);
+        dest.writeString(humidity);
+        dest.writeString(pressure);
+        dest.writeString(visibility);
+        dest.writeString(latitude);
+        dest.writeString(longitude);
+        dest.writeString(minimumTemperature);
+        dest.writeString(maximumTemperature);
+        dest.writeString(uvRisk);
+        dest.writeString(pollution);
+        dest.writeString(sunrise);
+        dest.writeString(sunset);
     }
 }

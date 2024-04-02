d6df94be4e87e60f71b5b1c8ddc4b52dec70eef8
githiomi <danielgithiomi@gmail.com>
2024-04-02 21:45:37 +0400

    Completed chart view flipper implementation

--- app/src/main/java/com/alu/skyesque/DetailsFragment.java
+++ app/src/main/java/com/alu/skyesque/DetailsFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import androidx.fragment.app.Fragment;
 
@@ -25,6 +27,8 @@ public class DetailsFragment extends Fragment {
 
     // Views
     TextView windDirection, uvIndex, rainfall, visibility;
+    ImageButton toPrevious, toNext;
+    ViewFlipper chartViewFlipper;
 
     public DetailsFragment() {
         // Required empty public constructor
@@ -49,16 +53,27 @@ public class DetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_details, container, false);
 
-        // Initialize views
-        this.windDirection = view.findViewById(R.id.TV_windDirection);
-        this.uvIndex = view.findViewById(R.id.TV_uvIndexDesc);
-        this.rainfall = view.findViewById(R.id.TV_rainfallDesc);
-        this.visibility = view.findViewById(R.id.TV_visibilityDesc);
+        // Bind views
+        bindViews(view);
 
         // Add data to fields
         populateWeatherData();
 
+        // Click Listeners - To toggle through charts
+        this.toPrevious.setOnClickListener(v -> this.chartViewFlipper.showPrevious());
+        this.toNext.setOnClickListener(v -> this.chartViewFlipper.showNext());
+
         return view;
+    }
+
+    private void bindViews(View view) {
+        this.toPrevious = view.findViewById(R.id.IV_toPrevious);
+        this.chartViewFlipper = view.findViewById(R.id.VF_chartFlipper);
+        this.toNext = view.findViewById(R.id.IV_toNext);
+        this.windDirection = view.findViewById(R.id.TV_windDirection);
+        this.uvIndex = view.findViewById(R.id.TV_uvIndexDesc);
+        this.rainfall = view.findViewById(R.id.TV_rainfallDesc);
+        this.visibility = view.findViewById(R.id.TV_visibilityDesc);
     }
 
     private void populateWeatherData() {

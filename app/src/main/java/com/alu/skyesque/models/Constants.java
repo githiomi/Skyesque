ed3419616ebfdefdd820b3482fa4925533380200
githiomi <danielgithiomi@gmail.com>
2024-04-30 11:01:12 +0400

    Added 6 more locations and their IDs

--- app/src/main/java/com/alu/skyesque/models/Constants.java
+++ app/src/main/java/com/alu/skyesque/models/Constants.java
@@ -25,7 +25,8 @@ public class Constants {
     public static final String MY_SHARED_PREFERENCES = "authentication";
     public static final String LOGGED_IN_USER = "Logged In User";
     public static final List<Location> LOCATIONS = new ArrayList<>(
-            Arrays.asList(Location.GLASGOW, Location.LONDON, Location.NEW_YORK, Location.OMAN, Location.MAURITIUS, Location.BANGLADESH)
+            Arrays.asList(Location.GLASGOW, Location.LONDON, Location.NEW_YORK, Location.OMAN, Location.MAURITIUS, Location.BANGLADESH,
+                    Location.CAIRO, Location.NAIROBI, Location.HARARE, Location.CAPE_TOWN, Location.ANTANANARIVO, Location.DAR_ES_SALAAM)
     );
 
 }
--- app/src/main/java/com/alu/skyesque/models/Location.java
+++ app/src/main/java/com/alu/skyesque/models/Location.java
@@ -15,10 +15,16 @@ public enum Location {
     NEW_YORK("New York",5128581L),
     OMAN("Oman",287286L),
     MAURITIUS("Mauritius",934154L),
-    BANGLADESH("Bangladesh",1185241L);
+    BANGLADESH("Bangladesh",1185241L),
+    CAPE_TOWN("Cape Town", 3369157L),
+    NAIROBI("Nairobi", 184745L),
+    ANTANANARIVO("Antananarivo", 1070940L),
+    DAR_ES_SALAAM("Dar es Salaam", 160263L),
+    HARARE("Harare", 890299L),
+    CAIRO("Cairo", 360630L);
 
-    private String locationName;
-    private Long locationId;
+    private final String locationName;
+    private final Long locationId;
 
     Location(String locationName, Long locationId) {
         this.locationName = locationName;
